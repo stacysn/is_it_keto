c37e0b1a8a8820a191f69892304daaa96743f331
stacysn <stacysuen5@gmail.com>
2018-03-14 15:31:55 -0700

    added main head/body components

---
+++ src/body/SearchContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+
+class SearchContainer extends Component {
+  render () {
+    return (
+      <h1> SearchContainer </h1>
+    )
+  }
+}
+export default SearchContainer;
---
+++ src/head/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+
+class Header extends Component {
+  render () {
+    return (
+      <h1> Keto Header </h1>
+    )
+  }
+}
+export default Header
