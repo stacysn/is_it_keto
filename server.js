56ae2fa51a96a8beb2f4c44608c9ae006acdf247
stacysn <stacysuen5@gmail.com>
2018-03-15 21:30:05 -0700

    added mlab db connection

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var router = express.Router();
 //set our port to either a predetermined port number if you have set
 //it up, or 3001
 var port = process.env.API_PORT || 3001;
+
+//db config
+mongoose.connect('mongodb://<blueCheese5>:<bacon5>@ds215019.mlab.com:15019/is-it-keto')
 //now we should configure the API to use bodyParser and look for
 //JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
