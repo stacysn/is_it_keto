dd7d17c1a34273edbfcfbc240c73648b0ab1e74e
dalazaro <daryl.lazaro@gmail.com>
2018-03-18 10:11:12 -0700

    wrote basic keto evaluation based on net carbs per serving

--- src/body/Results.js
+++ src/body/Results.js
@@ -3,27 +3,28 @@ import React, { Component } from "react";
 class Results extends Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      isKeto: null
-    };
   }
 
-  render () {
-    return (
+  render() {
+    return(
       <div>
         <h1>Results</h1>
         <div id="results">
-          {this.props.results.map((element, index) => {
-            let totalCarbs = this.props.results[index].nf_total_carbohydrate;
-            let dietaryFiber = this.props.results[index].nf_dietary_fiber;
-            let netCarbs = this.props.results[index].nf_total_carbohydrate - this.props.results[index].nf_dietary_fiber;
-            return (
+          {this.props.results.map( (element, index) => {
+            const foodName = (food) => { return food.charAt(0).toUpperCase() + food.slice(1); };
+            const totalCarbs = this.props.results[index].nf_total_carbohydrate;
+            const servingSize = `${this.props.results[index].serving_qty}  ${this.props.results[index].serving_unit}`;
+            const servingSizeGrams = this.props.results[index].serving_weight_grams;
+            const dietaryFiber = this.props.results[index].nf_dietary_fiber;
+            const netCarbs = this.props.results[index].nf_total_carbohydrate - this.props.results[index].nf_dietary_fiber;
+
+            return(
               <div key={index}>
-                <b>{this.props.results[index].food_name}</b>
-                <p>{totalCarbs}g Total Carbs</p>
-                <p>&mdash; {dietaryFiber}g Dietary Fiber</p>
-                <p>= {netCarbs}g Net Carbs</p>
+                <b>{foodName(this.props.results[index].food_name)} &mdash; {netCarbs < 10 ? 'Keto-friendly!': 'not Keto-friendly.'}</b>
+                <p>Serving size: {servingSize} ({servingSizeGrams} g)</p>
+                <p>{totalCarbs} g Total Carbs</p>
+                <p>&mdash; {dietaryFiber} g Dietary Fiber</p>
+                <p>= {netCarbs} g Net Carbs</p>
               </div>
             )
           })}
@@ -32,4 +33,5 @@ class Results extends Component {
     );
   }
 }
+
 export default Results;
