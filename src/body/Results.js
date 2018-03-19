169f7a862b41f56e5f0c4807d8670d7129a8d58e
dalazaro <daryl.lazaro@gmail.com>
2018-03-19 13:34:47 -0700

    wrote helper functions foodName and isKeto

--- src/body/Results.js
+++ src/body/Results.js
@@ -3,6 +3,24 @@ import React, { Component } from "react";
 class Results extends Component {
   constructor(props, context) {
     super(props, context);
+    this.foodName = this.foodName.bind(this);
+    this.isKeto = this.isKeto.bind(this);
+  }
+
+  foodName(food) {
+    return food.charAt(0).toUpperCase() + food.slice(1);
+  }
+
+  isKeto(netCarbs) {
+    if (netCarbs < 10) {
+      return(
+        <span>Keto-friendly!</span>
+      )
+    } else {
+      return(
+        <span>Not Keto-friendly.</span>
+      )
+    }
   }
 
   render() {
@@ -11,7 +29,6 @@ class Results extends Component {
         <h1>Results</h1>
         <div id="results">
           {this.props.results.map( (element, index) => {
-            const foodName = (food) => { return food.charAt(0).toUpperCase() + food.slice(1); };
             const totalCarbs = this.props.results[index].nf_total_carbohydrate;
             const servingSize = `${this.props.results[index].serving_qty}  ${this.props.results[index].serving_unit}`;
             const servingSizeGrams = this.props.results[index].serving_weight_grams;
@@ -19,8 +36,8 @@ class Results extends Component {
             const netCarbs = this.props.results[index].nf_total_carbohydrate - this.props.results[index].nf_dietary_fiber;
 
             return(
-              <div key={index}>
-                <b>{foodName(this.props.results[index].food_name)} &mdash; {netCarbs < 10 ? 'Keto-friendly!': 'not Keto-friendly.'}</b>
+              <div key={`food-idx-${index}`}>
+                <b>{this.foodName(this.props.results[index].food_name)} &mdash; {this.isKeto(netCarbs)}</b>
                 <p>Serving size: {servingSize} ({servingSizeGrams} g)</p>
                 <p>{totalCarbs} g Total Carbs</p>
                 <p>&mdash; {dietaryFiber} g Dietary Fiber</p>
