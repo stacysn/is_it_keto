18199ad23c8177455196a269624c5bcfd79eb1c7
dalazaro <daryl.lazaro@gmail.com>
2018-03-19 13:35:01 -0700

    added todo

--- src/body/SearchContainer.js
+++ src/body/SearchContainer.js
@@ -34,6 +34,7 @@ class SearchContainer extends Component {
         console.log('Error: ' + response.status);
         return;
       }
+      // TODO: .fail this console log
 
       response.json().then(data => {
         const results = data.foods;
