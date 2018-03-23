7bfba7ab32923ab599b9e5d0ea92725a3134dea7
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-03-22 22:05:32 -0700

    created Profile page component and setup routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./assets/styles/App.css";
+import { Switch, Route } from "react-router-dom";
+
 import Home from "./pages/Home.js";
-import { Switch, Route } from 'react-router-dom'
+import Profile from "./pages/Profile.js";
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/profile" component={Profile} />
       </Switch>
     );
   }
