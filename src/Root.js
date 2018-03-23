84b4a88e3be801ffb7c527fafef5fe4b7a972dd9
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-03-22 21:23:11 -0700

    created Root.js component for routing

---
+++ src/Root.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Link } from "react-router";
+import { Home } from "../../Containers";
+
+export default class Root extends Component {
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+}
