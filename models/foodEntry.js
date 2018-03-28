8cc13472e7d4fafe3187d5d9124b5bce5a610693
dalazaro <daryl.lazaro@gmail.com>
2018-03-28 15:04:31 -0700

    created models and updated server.js

---
+++ models/foodEntry.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose"),
+  Schema = mongoose.Schema;
+
+const FoodEntrySchema = new Schema({
+  foodData: Object
+})
+
+const FoodEntry = mongoose.model('FoodEntry', FoodEntrySchema);
+
+module.exports = FoodEntry;
