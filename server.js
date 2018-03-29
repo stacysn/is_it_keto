1147f2cc2c25f8f38f666bf4942e660217566ad0
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-03-28 20:14:12 -0700

    cleaned up server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 "use strict";
-//first we import our dependencies…
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
@@ -10,10 +9,10 @@ const User = require("./models/user");
 //Controllers
 const userController = require("./controllers/userController");
 
-//and create our instances
 var app = express();
 var router = express.Router();
 
+//mLab
 const mongoDB = "mongodb://keto:bacon5@ds215019.mlab.com:15019/is-it-keto";
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
@@ -21,12 +20,8 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 var port = process.env.API_PORT || 3001;
-//now we should configure the API to use bodyParser and look for
-//JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//To prevent errors from Cross Origin Resource Sharing, we will set
-//our headers to allow CORS with middleware like so:
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
@@ -38,11 +33,13 @@ app.use(function(req, res, next) {
     "Access-Control-Allow-Headers",
     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
-  //and remove cacheing so we get the most recent comments
   res.setHeader("Cache-Control", "no-cache");
   next();
 });
-//now we can set the route path & initialize the API
+
+/* ROUTES */
+
+//Initialize the API
 router.get("/", function(req, res) {
   res.json({ message: "API Initialized!" });
 });
@@ -53,9 +50,7 @@ router
   .get(userController.userGet)
   .post(userController.userSignUp);
 
-//Use our router configuration when we call /api
 app.use("/api", router);
-//starts the server and listens for requests
 app.listen(port, function() {
   console.log("api running on port " + port);
 });
