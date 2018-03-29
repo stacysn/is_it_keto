ed2033cc2239994129e8f7593ffed0d3526ab8ad
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-03-28 20:19:30 -0700

    created Login component

---
+++ src/head/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../assets/styles/Login.css";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h3 className="login">Login</h3>;
+  }
+}
+
+export default Login;
