540ef4c71c7a2b9683d193210368382876af2ed6
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-03-28 18:38:25 -0700

    built out basic signup page

---
+++ src/pages/SignUp.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from "../head/Header.js";
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+export default SignUp;
