2e6efcdef9f2e62b5f1a1831780752ba5629d37a
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-03-28 18:37:36 -0700

    created SignUp page component

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-'use strict'
+"use strict";
 //first we import our dependencies…
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+const User = require('./models/user');
+
 //and create our instances
 var app = express();
 var router = express.Router();
-//set our port to either a predetermined port number if you have set
-//it up, or 3001
+
+const mongoDB = "mongodb://keto:bacon5@ds215019.mlab.com:15019/is-it-keto";
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 var port = process.env.API_PORT || 3001;
 //now we should configure the API to use bodyParser and look for
 //JSON data in the request body
@@ -16,21 +24,46 @@ app.use(bodyParser.json());
 //To prevent errors from Cross Origin Resource Sharing, we will set
 //our headers to allow CORS with middleware like so:
 app.use(function(req, res, next) {
- res.setHeader('Access-Control-Allow-Origin', '*');
- res.setHeader('Access-Control-Allow-Credentials', 'true');
- res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
- res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-//and remove cacheing so we get the most recent comments
- res.setHeader('Cache-Control', 'no-cache');
- next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  //and remove cacheing so we get the most recent comments
+  res.setHeader("Cache-Control", "no-cache");
+  next();
 });
 //now we can set the route path & initialize the API
-router.get('/', function(req, res) {
- res.json({ message: 'API Initialized!'});
+router.get("/", function(req, res) {
+  res.json({ message: "API Initialized!" });
 });
+
+//user routes
+router
+  .route("/users")
+  .get(function(req, res) {
+    User.find(function(err, users) {
+      if (err) res.send(err);
+      res.json(users);
+    });
+  })
+  .post(function(req, res) {
+    let user = new User();
+    user.name = "Ryan";
+    user.save(function(err) {
+      if (err) res.send(err);
+      res.json({ message: "User successfully added!" });
+    });
+  });
+
 //Use our router configuration when we call /api
-app.use('/api', router);
+app.use("/api", router);
 //starts the server and listens for requests
 app.listen(port, function() {
- console.log('api running on port ' + port);
+  console.log("api running on port " + port);
 });
