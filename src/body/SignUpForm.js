9a9adf8c5a85c5edf11ba5d124c704febc014edf
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-03-28 18:44:26 -0700

    imported SignUpForm component into SignUp page

---
+++ src/body/SignUpForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1Form</h1>;
+  }
+}
+
+export default SignUpForm;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "../head/Header.js";
+import SignUpForm from "../body/SignUpForm.js";
 
 class SignUp extends Component {
   render() {
     return (
       <div>
         <Header />
+        <SignUpForm />
       </div>
     );
   }
