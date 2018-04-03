00c51f02a1110709df2e630181006fdbb8e268ac
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-04-02 19:26:53 -0700

    created event handler for new foodData entry

--- src/body/food/FoodIndexItem.js
+++ src/body/food/FoodIndexItem.js
@@ -2,6 +2,23 @@ import React, { Component } from "react";
 import "../../assets/styles/FoodIndexItem.css";
 
 class FoodIndexItem extends Component {
+  handleNewEntry = event => {
+    event.preventDefault();
+    fetch("http://localhost:3001/api/foodEntry", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(this.props)
+    }).then(response => {
+      return response.json().then(json => {
+        if (response.ok) {
+          console.log("New Entry Created!");
+        } else {
+          console.log("Failed to create new entry =[");
+        }
+      });
+    });
+  };
+
   render() {
     const {
       dietaryFiber,
@@ -14,16 +31,21 @@ class FoodIndexItem extends Component {
     } = this.props;
     if (this.props.isLoginSuccess === true) {
       return (
-        <div className="food-idx-item">
-          <b>
-            {foodName} &mdash; {isKeto}
-          </b>
-          <p>
-            Serving size: {servingSize} ({servingSizeGrams} g)
-          </p>
-          <p>{totalCarbs} g Total Carbs</p>
-          <p>&mdash; {dietaryFiber} g Dietary Fiber</p>
-          <p>= {netCarbs} g Net Carbs</p>
+        <div>
+          <div className="food-idx-item">
+            <b>
+              {foodName} &mdash; {isKeto}
+            </b>
+            <p>
+              Serving size: {servingSize} ({servingSizeGrams} g)
+            </p>
+            <p>{totalCarbs} g Total Carbs</p>
+            <p>&mdash; {dietaryFiber} g Dietary Fiber</p>
+            <p>= {netCarbs} g Net Carbs</p>
+          </div>
+          <button type="submit" onSubmit={this.handleNewEntry}>
+            Add to Profile
+          </button>
         </div>
       );
     } else {
