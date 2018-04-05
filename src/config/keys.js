7110dd3ca69091d788906176a43403cc24740068
stacysn <stacysuen5@gmail.com>
2018-04-05 15:08:31 -0700

    divided form and hrefs in header

---
+++ src/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  nutritionixKey: "381d57068dd3a75a7f6dc82f3b275f65",
+  nutritionixAppId: "2e1bc7f0"
+};
--- src/head/Header.js
+++ src/head/Header.js
@@ -6,16 +6,27 @@ import "../assets/styles/Header.css";
 
 class Header extends Component {
   render() {
-    return (
+    if (this.props.isLoginSuccess !== true) {
+      return (
+        <div className = "header">
+          <a href="/"> Is It Keto? </a>
+        <LoginForm />
+        <a href="/signup">Sign Up</a>
+
+        </div>
+
+      )
+    }
+    return(
       <div className = "header">
         <a href="/"> Is It Keto? </a>
           <nav className='nav-list'>
             <a href="/profile">My Profile</a>
             <a href="/signup">Sign Up</a>
           </nav>
-          <LoginForm />
       </div>
-    );
+    )
+
   }
 }
 export default Header;
