99a5957a8279b2746fb14ff94164e35fff9ea298
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-04-07 21:11:26 -0700

    adjusted login placement in header

--- src/head/LoginForm.js
+++ src/head/LoginForm.js
@@ -21,47 +21,54 @@ class LoginForm extends Component {
         <div>
           <nav className="nav-list">
             <a href="/profile">My Profile</a>
-            <a href="/home" type="submit" onClick={this.logout}>Logout</a>
+            <a href="/home" type="submit" onClick={this.logout}>
+              Logout
+            </a>
           </nav>
         </div>
-      )
+      );
     } else {
       return (
-        <form name="loginForm" id="loginForm"onSubmit={this.onSubmit}>
-          <div className="form-group-collection">
-            <div className="form-group">
-              <label>UserName:</label>
-              <input
-                name="userName"
-                onChange={e => this.setState({ userName: e.target.value })}
-                value={userName}
-              />
+        <div>
+          <nav className="nav-list signup-nav">
+            <a href="/signup">Sign Up</a>
+          </nav>
+          <form name="loginForm" id="loginForm" onSubmit={this.onSubmit}>
+            <div className="form-group-collection">
+              <div className="form-group login-form-username">
+                <label>UserName:</label>
+                <input
+                  name="userName"
+                  onChange={e => this.setState({ userName: e.target.value })}
+                  value={userName}
+                />
+              </div>
+
+              <div className="form-group login-form-password">
+                <label>Password:</label>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={e => this.setState({ password: e.target.value })}
+                  value={password}
+                />
+              </div>
             </div>
 
-            <div className="form-group">
-              <label>Password:</label>
-              <input
-                type="password"
-                name="password"
-                onChange={e => this.setState({ password: e.target.value })}
-                value={password}
-              />
+            <input type="submit" value="Login" className="login-button" />
+
+            <div className="message">
+              {isLoginPending && <div>Please wait...</div>}
+              {isLoginSuccess && <div>Success.</div>}
+              {loginError && <div>{loginError.message}</div>}
             </div>
-          </div>
-
-          <input type="submit" value="Login" />
-
-          <div className="message">
-            {isLoginPending && <div>Please wait...</div>}
-            {isLoginSuccess && <div>Success.</div>}
-            {loginError && <div>{loginError.message}</div>}
-          </div>
-        </form>
+          </form>
+        </div>
       );
     }
   }
 
-  onSubmit = function(e){
+  onSubmit = function(e) {
     e.preventDefault();
     let { userName, password } = this.state;
     this.props.login(userName, password);
@@ -69,15 +76,13 @@ class LoginForm extends Component {
       userName: "",
       password: ""
     });
-  }
+  };
 
-  logout = function(e){
+  logout = function(e) {
     e.preventDefault();
     this.props.logout(false);
-  }
+  };
 }
-
-
 
 const mapStateToProps = state => {
   return {
