02608d4d577665cc4e25214c2250b7b643dc6d8b
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-04-12 13:49:25 -0700

    rendered basic svg to profile page

--- src/body/ProfileContainer.js
+++ src/body/ProfileContainer.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
+import { Element } from "react-faux-dom";
+import * as d3 from "d3";
 import "../assets/styles/Profile.css";
-import BarChart from "./charts/BarChart";
-import FoodResults from "./food/FoodResults.js"
-import FoodSearchContainer from "./food/FoodSearchContainer.js"
+import FoodResults from "./food/FoodResults.js";
+import FoodSearchContainer from "./food/FoodSearchContainer.js";
 
 class ProfileContainer extends Component {
   constructor(props) {
     super(props);
   }
 
+  drawChart() {
+    const width = 800;
+    const height = 450;
+    const el = new Element("div");
+    const svg = d3
+      .select(el)
+      .append("svg")
+      .attr("id", "chart")
+      .attr("width", width)
+      .attr("height", height);
+
+    return el.toReact();
+  }
+
   render() {
     return (
       <div className="profile-container">
         <h1>My Profile</h1>
-          <FoodSearchContainer />
-          <BarChart data={[5,10,1,3]} size={[500,500]} />
+        {this.drawChart()};
+        <FoodSearchContainer />
       </div>
-    )
+    );
   }
 }
 
 export default ProfileContainer;
+        
