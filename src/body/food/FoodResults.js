924638997d1df4b619e8c9005e68b518c5559b2a
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-04-18 17:58:30 -0700

    refactored FoodResults.js to use arrow functions for scope binding

--- src/body/food/FoodResults.js
+++ src/body/food/FoodResults.js
@@ -1,50 +1,53 @@
 import React, { Component } from "react";
 import FoodIndexItem from "./FoodIndexItem";
 import "../../assets/styles/FoodResults.css";
-import ProfileContainer from '../ProfileContainer.js'
+import ProfileContainer from "../ProfileContainer.js";
 
 class FoodResults extends Component {
   constructor(props, context) {
     super(props, context);
-    this.foodName = this.foodName.bind(this);
-    this.isKeto = this.isKeto.bind(this);
   }
 
-  foodName(food) {
+  foodName = food => {
     return food.charAt(0).toUpperCase() + food.slice(1);
-  }
+  };
 
-  isKeto(netCarbs) {
-    return(
-      netCarbs < 10 ? <span>Keto-friendly!</span> : <span>Not Keto-friendly.</span>
-    )
-  }
+  isKeto = netCarbs => {
+    return netCarbs < 10 ? (
+      <span>Keto-friendly!</span>
+    ) : (
+      <span>Not Keto-friendly.</span>
+    );
+  };
 
   render() {
     return (
       <div className="food-results-container">
         <h1>FoodResults</h1>
         <div id="food-results">
-          {this.props.results.map( (element, index) => {
+          {this.props.results.map((element, index) => {
             const totalCarbs = element.nf_total_carbohydrate;
-            const servingSize = `${element.serving_qty}  ${element.serving_unit}`;
+            const servingSize = `${element.serving_qty}  ${
+              element.serving_unit
+            }`;
             const servingSizeGrams = element.serving_weight_grams;
             const dietaryFiber = element.nf_dietary_fiber;
-            const netCarbs = (element.nf_total_carbohydrate - element.nf_dietary_fiber).toFixed(2);
+            const netCarbs = (
+              element.nf_total_carbohydrate - element.nf_dietary_fiber
+            ).toFixed(2);
 
-            return(
+            return (
               <FoodIndexItem
-                dietaryFiber={ dietaryFiber }
-                foodName={ this.foodName(element.food_name) }
-                isKeto={ this.isKeto(netCarbs) }
-                key={ `food-idx-${index}` }
-                netCarbs={ netCarbs }
-                servingSize={ servingSize }
-                servingSizeGrams={ servingSizeGrams }
-                totalCarbs={ totalCarbs }
+                dietaryFiber={dietaryFiber}
+                foodName={this.foodName(element.food_name)}
+                isKeto={this.isKeto(netCarbs)}
+                key={`food-idx-${index}`}
+                netCarbs={netCarbs}
+                servingSize={servingSize}
+                servingSizeGrams={servingSizeGrams}
+                totalCarbs={totalCarbs}
               />
-            )
-            
+            );
           })}
         </div>
       </div>
