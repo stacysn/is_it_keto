c44ed2ef4bbf99d4f6f448e055863bb326c9cc65
stacysn <stacysuen5@gmail.com>
2018-04-29 14:08:56 -0700

    added parallax to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,26 @@ import React, { Component } from "react";
 import Header from "../head/Header.js";
 import FoodSearchContainer from "../body/food/FoodSearchContainer.js";
 import ExerciseSearchContainer from "../body/exercise/ExerciseSearchContainer.js";
+import { Parallax, Background } from 'react-parallax';
+
 
 class Home extends Component {
   render() {
     return (
       <div className="App">
+      <div>
+        <Parallax
+          bgImage={require('../assets/images/lemon.jpeg')}
+          bgImageAlt="the lemon"
+          strength={200}
+        >
+
+          <div style={{ height: '600px' }} />
+        </Parallax>
+      </div>
+
         <Header />
+
         <FoodSearchContainer />
         <ExerciseSearchContainer />
       </div>
