f693157802e529579f4e2aef1372c51aba3fcdaa
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-05-16 17:00:01 -0700

    removed endpoints to display all users / entries

--- controllers/FoodController.js
+++ controllers/FoodController.js
@@ -1,14 +1,6 @@
 const FoodEntry = require("../models/foodEntry.js");
 const User = require("../models/user.js");
 
-//for testing only
-exports.entryGet = function(req, res) {
-  FoodEntry.find(function(err, entries) {
-    if (err) res.send(err);
-    res.json(entries);
-  });
-};
-
 exports.newEntry = function(req, res) {
   let entry = new FoodEntry();
   entry.foodData = req.body.foodData;
