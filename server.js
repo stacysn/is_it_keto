50bb74224ade2d82295df92f48ff8d2a757e3fdd
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-05-16 17:06:16 -0700

    fixed error with heroku code in server.js

--- server.js
+++ server.js
@@ -68,12 +68,12 @@ app.use("/api", router);
 
 if (process.env.NODE_ENV === "production") {
   //express will serve production assets
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
 
   //express will serve index.html file if it does not know the route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
